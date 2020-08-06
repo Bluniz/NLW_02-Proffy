5d5275c8f4beb1e109339ca72990ae73e68fbbbb
Lucas Rosa <55213609+Bluniz@users.noreply.github.com>
2020-08-06 12:08:54 -0300

    Aula 3: Finalizando o FrontEnd

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,36 +1,53 @@
 import React from 'react';
 import './styles.css';
 import wppIcon from '../../assets/images/icons/whatsapp.svg'
+import api from '../../services/api';
+
+export interface Teacher{
+    id: number,
+    avatar: string,
+    bio: string,
+    cost: number,
+    name: string,
+    subject: string,
+    whatsapp: string
+}
+
+  interface TeacherItemProps{
+  teacher: Teacher
+}
 
 
-
-
-function TeacherItem(){
+const TeacherItem: React.FC<TeacherItemProps> =({teacher}) =>{
+  function createNewConnection(){
+    api.post('connections', {user_id: teacher.id});
+  }
   return(
     <article className="teacher-item">
          <header>
-           <img src="https://pbs.twimg.com/profile_images/1263639597076144130/GrA8GPFc_400x400.jpg" alt="Lucas Rosda"/>
+           <img src={teacher.avatar} alt={teacher.name}/>
            <div>
-             <strong>Lucas Rosa</strong>
-             <span>Química</span>
+  <strong>{teacher.name}</strong>
+  <span>{teacher.subject}</span>
            </div>
          </header>
 
          <p>
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-           <br/><br/>
-           Sequi odio architecto dignissimos nostrum nulla aperiam nobis. Maxime molestiae corporis incidunt non veniam porro vitae nulla? Totam, earum? Repellat, assumenda eos!
+          {teacher.bio}
          </p>
 
          <footer>
            <p>
              Preço/Hora:
-             <strong>R$ 100,00</strong>
+  <strong>R$ {teacher.cost}</strong>
            </p>
            <button type="button">
+             <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} >
              <img src={wppIcon} alt="Whatsapp"/>
              Entrar em contato
+             </a>
            </button>
+           
          </footer>
        </article>
   )
