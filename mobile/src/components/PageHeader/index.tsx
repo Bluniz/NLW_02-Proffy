22f30e4689c12410d411ebcb3c4fc21dcff8fc89
Lucas Rosa <55213609+Bluniz@users.noreply.github.com>
2020-08-08 20:15:21 -0300

    Aula 05: FINALIZADO!

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import styles from './styles';
 import {BorderlessButton} from 'react-native-gesture-handler'
@@ -10,10 +10,11 @@ import { useNavigation } from '@react-navigation/native';
 
 interface PageHeaderProps{
   title: string;
+  headerRight?: ReactNode;
 }
 
 
-const PageHeader: React.FC<PageHeaderProps> = ({title}) =>{
+const PageHeader: React.FC<PageHeaderProps> = ({title, children, headerRight}) =>{
   const {navigate} = useNavigation();
 
   function handleGoBack(){
@@ -31,8 +32,14 @@ const PageHeader: React.FC<PageHeaderProps> = ({title}) =>{
       <Image source={logoImg} resizeMode="contain"/>
     </View>
 
+   <View style={styles.header}>
 
-  <Text style={styles.title}>{title}</Text>
+     <Text style={styles.title}>{title}</Text>
+
+     {headerRight}
+  </View>
+
+  {children}
 
 
   </View>
