42630aa1621d53bef6c7787915e8b51d096d9f99
Lucas Rosa <55213609+Bluniz@users.noreply.github.com>
2020-08-07 22:23:59 -0300

    Aula 4

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View } from 'react-native';
+import styles from './styles';
+import PageHeader from '../../components/PageHeader/index';
+
+
+
+function Favorites(){
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Meus proffys favoritos"/>
+      
+      </View>
+      )
+}
+
+
+
+
+
+export default Favorites;
